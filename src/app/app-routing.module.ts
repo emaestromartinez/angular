ff485208084863b20183c64463aef0fb6470cc80
emaestro <eduard.maestro@elma.care>
2022-12-01 10:54:07 +0100

    Created initial routing and home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,39 @@
+import { HomeComponent } from './pages/home/home.component';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, Route } from '@angular/router';
 
 const routes: Routes = [];
 
+export interface IRouteData {
+  breadcrumb?: string;
+}
+
+export interface CustomRoute extends Route {
+  data?: IRouteData;
+}
+
+export const routesApp: CustomRoute[] = [
+  {
+    path: '',
+    component: HomeComponent,
+    children: [
+      // {
+      //   path: '',
+      //   loadChildren: () =>
+      //     import('./pages/home/home.module').then((m) => m.HomeModule),
+      // },
+      // { path: '**', component: NotFoundComponent },
+    ],
+  },
+  // { path: '404', component: NotFoundComponent },
+  // { path: '**', component: NotFoundComponent },
+];
+
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routesApp, { scrollPositionRestoration: 'disabled' }),
+  ],
+
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
