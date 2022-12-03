4a0dc5eb43e030f866e68292bcdab4ab0f5d4090
emaestro <eduard.maestro@elma.care>
2022-12-03 17:21:13 +0100

    added button to come back to form

--- src/app/pages/second-page/second-page.module.ts
+++ src/app/pages/second-page/second-page.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SecondPageComponent } from './second-page.component';
 import { SecondPageRoutingModule } from './second-page-routing.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [CommonModule, SecondPageRoutingModule],
+  imports: [CommonModule, RouterModule, SecondPageRoutingModule],
   declarations: [SecondPageComponent],
 })
 export class SecondPageModule {}
