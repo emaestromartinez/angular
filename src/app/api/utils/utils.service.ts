1e077c6ae3521eb83723691c9251a655d9f4a041
emaestro <eduard.maestro@elma.care>
2025-04-01 17:07:39 +0200

    added utils service

---
+++ src/app/api/utils/utils.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable, from, catchError, throwError } from 'rxjs';
+
+import { mergeMap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiService {
+  constructor(private http: HttpClient) {}
+
+  // Función que recibe un array de URLs y el límite de concurrencia
+  fetchUrls(urls: string[], maxConcurrency: number): Observable<any[]> {
+    return from(urls).pipe(
+      mergeMap((url) => this.http.get(url), maxConcurrency)
+    );
+  }
+}
